2488c900a5e06828c03f3013de9552a8d7d7b6c3
Fabio Morais <fabio.lmorais99@gmail.com>
2024-05-28 20:06:05 -0300

    Feat: Criado a busca paginada

--- src/main/java/com/desafio/clients_crud/controllers/ClientController.java
+++ src/main/java/com/desafio/clients_crud/controllers/ClientController.java
@@ -3,6 +3,8 @@ package com.desafio.clients_crud.controllers;
 import com.desafio.clients_crud.dto.ClientDTO;
 import com.desafio.clients_crud.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,5 +22,11 @@ public class ClientController {
     public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
        ClientDTO dto = service.findById(id);
        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ClientDTO>> findAll (Pageable pageable) {
+        Page<ClientDTO> dto = service.findAll(pageable);
+        return ResponseEntity.ok(dto);
     }
 }
