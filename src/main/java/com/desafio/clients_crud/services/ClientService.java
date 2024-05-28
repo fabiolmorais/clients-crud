1482c7af957f8fd70e71e301a533584ca504d1e0
Fabio Morais <fabio.lmorais99@gmail.com>
2024-05-28 19:58:32 -0300

    Feat: Criado a busca de recurso por id

--- src/main/java/com/desafio/clients_crud/services/ClientService.java
+++ src/main/java/com/desafio/clients_crud/services/ClientService.java
@@ -1,12 +1,21 @@
 package com.desafio.clients_crud.services;
 
+import com.desafio.clients_crud.dto.ClientDTO;
+import com.desafio.clients_crud.entities.Client;
 import com.desafio.clients_crud.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ClientService {
 
     @Autowired
     private ClientRepository repository;
+
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id) {
+        Client client = repository.findById(id).get();
+        return new ClientDTO(client);
+    }
 }
